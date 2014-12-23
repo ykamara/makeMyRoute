214d3ab013594396ed330ba469eff854c7cba73e
mirafra <mirafra@mirafra-ThinkCentre-A70.(none)>
2014-12-23 15:54:27 +0530

    home screen initial checkin

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,14 @@
-var Map = require('ti.map');
+var data = [];
+data[0]=Ti.UI.createPickerRow({title:'Select Your City',backgroundColor : 'gray'});
+data[1]=Ti.UI.createPickerRow({title:'Bangalore',backgroundColor : 'gray'});
+data[2]=Ti.UI.createPickerRow({title:'Mysoor',backgroundColor : 'gray'});
+data[3]=Ti.UI.createPickerRow({title:'Mangalore',backgroundColor : 'gray'});
 
-var mapview = Map.createView({
-    mapType: Map.NORMAL_TYPE,
-   // region:{latitude:33.74511, longitude:-84.38993, latitudeDelta:0.5, longitudeDelta:0.5},
-    animate:true,
-    regionFit:true,
-    userLocation:true
-});
-
-$.mapView.add(mapview);
-
+var places=[];
+places[0]=Ti.UI.createTableViewRow({title:'Marthahalli',color:'black'});
+places[1]=Ti.UI.createTableViewRow({title:'Itpl',color:'black'});
+places[2]=Ti.UI.createTableViewRow({title:'Magistic',color:'black'});
+places[3]=Ti.UI.createTableViewRow({title:'Silk bord',color:'black'});
+$.table.data=places;
+$.picker.add(data);
 $.index.open();
